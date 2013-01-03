c00e305d493bef60bf9ccf6c46d2dc7ef980f4d4
Ronny Standtke <ronny.standtke@fhnw.ch>
2013-01-03 19:41:43 +0100

    added example for Debian 7

---
+++ examples/lernstick_debian7/update_po.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+itstool -o xmlboot.pot xmlboot.config
+
+for i in po/*.po; do
+
+	# update po file
+	msgmerge -U $i xmlboot.pot
+
+	# convert po file to null-separated format
+	po2csv -i $i | \
+	sed '1,1d' | \
+	sed -e 's|"\(.*\)","\(.*\)","\(.*\)"|\2\x00\3|' | \
+	tr '\n' '\000' | \
+	tr -d '\r' > $(echo $i | sed 's|\.po||').translation
+done
+
+rm xmlboot.pot
